64e2704814574de0234d0282bb20bf3866ce6309
Vani Nagulapally <vanireddy0756@gmail.com>
2024-02-09 14:15:14 -0600

    programs

---
+++ src/main/java/streams/DuplicateRemover.java
@@ -0,0 +1,12 @@
+package streams;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class DuplicateRemover {
+
+    public static List<Integer> removeDuplicates(List<Integer> list) {
+        return list.stream()
+                .distinct()
+                .collect(Collectors.toList());
+    }
+}
---
+++ src/main/java/streams/StringLengthGrouper.java
@@ -0,0 +1,10 @@
+package streams;
+import java.util.*;
+import java.util.stream.Collectors;
+public class StringLengthGrouper {
+
+    public static Map<Integer, List<String>> groupByLength(List<String> strings) {
+        return strings.stream()
+                .collect(Collectors.groupingBy(String::length));
+    }
+}
